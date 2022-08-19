4b77296f959585009c6a368d4081413ad01f496e
Adrian Nilsson <adnilsson@users.noreply.github.com>
2022-08-20 00:14:02 +0200

    First solution

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -1,11 +1,19 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
+const CARS_PER_HOUR: u32 = 221;
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let maximum_rate = CARS_PER_HOUR * speed as u32;
+    let success_rate = match speed {
+        1..=4 => 1.0,
+        5..=8 => 0.90,
+        9 | 10 => 0.77,
+        _ => 0.0,
+    };
+
+    maximum_rate as f64 * success_rate
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    let hourly_rate = production_rate_per_hour(speed);
+
+    (hourly_rate / 60.0) as u32
 }

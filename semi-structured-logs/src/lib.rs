3eb661c2d9e2183c2046d724475e58ec9fcf5446
Adrian Nilsson <adnilsson@users.noreply.github.com>
2022-08-20 14:34:02 +0200

    Solve by implementing the Display trait
    This way, the log function is a simple call to the format macro.
    Maybe over engineered for the purpose of this exercise; one could just do
    the uppercase string conversion directly in the log function.

--- semi-structured-logs/src/lib.rs
+++ semi-structured-logs/src/lib.rs
@@ -1,24 +1,34 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
+use std::fmt;
 
 /// various log levels
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum LogLevel {
+    Debug,
     Info,
     Warning,
     Error,
 }
+
+impl fmt::Display for LogLevel {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let level_str = format!("{self:?}").to_uppercase();
+        write!(f, "{}", level_str)
+    }
+}
+
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!("return a message for the given log level")
+    format!("[{}]: {}", level, message)
+}
+pub fn debug(message: &str) -> String {
+    log(LogLevel::Debug, message)
 }
 pub fn info(message: &str) -> String {
-    unimplemented!("return a message for info log level")
+    log(LogLevel::Info, message)
 }
 pub fn warn(message: &str) -> String {
-    unimplemented!("return a message for warn log level")
+    log(LogLevel::Warning, message)
 }
 pub fn error(message: &str) -> String {
-    unimplemented!("return a message for error log level")
+    log(LogLevel::Error, message)
 }

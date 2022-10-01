ccf81b46b0999ec0ce34e6a4216bff46a3641f2f
Adrian Nilsson <adnilsson@users.noreply.github.com>
2022-10-01 15:56:17 +0200

    First solution to the Magazine Cutout learning exercise

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -1,9 +1,33 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 use std::collections::HashMap;
 
+fn word_count<'a>(words: &[&'a str]) -> HashMap<&'a str, u32> {
+    let mut counts = HashMap::with_capacity(words.len());
+    for word in words {
+        counts.entry(*word).and_modify(|e| *e += 1).or_insert(1);
+    }
+    counts
+}
+
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    // Smiple heuristic: it's impossible to build 'note' from 'magazine'
+    // if the note has more words than the magazine.
+    if note.len() > magazine.len() {
+        return false;
+    }
+
+    let cutouts = word_count(magazine);
+    let required_words = word_count(note);
+
+    for (note_word, note_count) in required_words {
+        match cutouts.get(note_word) {
+            Some(cutout_count) => {
+                if *cutout_count < note_count {
+                    return false;
+                }
+            }
+            None => return false,
+        }
+    }
+
+    true
 }

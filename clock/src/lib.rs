dcc2c0697f17a2c0abaabb01215dcfe363ae934b
Adrian Nilsson <adnilsson@users.noreply.github.com>
2023-01-05 23:05:01 +0100

    First solution to the Clock exercise

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,27 @@
-pub struct Clock;
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    minutes: u16,
+}
+
+const MAX_MINUTES: u16 = 24 * 60;
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Self { minutes: 0 }.add_minutes(60 * hours + minutes)
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        let minutes_sum = minutes + self.minutes as i32;
+        Self {
+            minutes: minutes_sum.rem_euclid(MAX_MINUTES as i32) as u16,
+        }
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.minutes / 60, self.minutes % 60)
     }
 }

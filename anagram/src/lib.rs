bb9c6b79dd4924be309bdb43dced5a9abcac98d6
Adrian Nilsson <adnilsson@users.noreply.github.com>
2023-02-12 13:52:53 +0100

    First solution to the Anagram exercise

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,7 +1,27 @@
 use std::collections::HashSet;
+use unicode_segmentation::UnicodeSegmentation;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{word}' word find anagrams among the following words: {possible_anagrams:?}"
-    );
+fn grapheme_sort(word: &str) -> String {
+    let mut grapheme_clusters: Vec<&str> = UnicodeSegmentation::graphemes(word, true).collect();
+    grapheme_clusters.sort_unstable();
+
+    grapheme_clusters.join("")
+}
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let lowered_word = word.to_lowercase();
+    let target_word = grapheme_sort(&lowered_word);
+
+    let lowered_candidate_words: Vec<String> =
+        possible_anagrams.iter().map(|w| w.to_lowercase()).collect();
+    let lowered_and_sorted = lowered_candidate_words.iter().map(|x| grapheme_sort(&x));
+
+    lowered_and_sorted
+        .enumerate()
+        .fold(HashSet::new(), |mut acc, (i, candidate)| {
+            if candidate == target_word && lowered_word != lowered_candidate_words[i] {
+                acc.insert(possible_anagrams[i]);
+            }
+            acc
+        })
 }

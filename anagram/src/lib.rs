e6e36f109e13c5a162448258652d7814e8842e5c
Adrian Nilsson <adnilsson@users.noreply.github.com>
2023-02-12 14:15:56 +0100

    Simplifications inspired by bobahop's solution
    The key is the power of .collect(), which can directly convert the
    Iterator to a HashSet.
    No need to build the HashSet manually!

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,27 +1,22 @@
 use std::collections::HashSet;
 use unicode_segmentation::UnicodeSegmentation;
 
-fn grapheme_sort(word: &str) -> String {
+fn grapheme_sort(word: &str) -> Vec<&str> {
     let mut grapheme_clusters: Vec<&str> = UnicodeSegmentation::graphemes(word, true).collect();
     grapheme_clusters.sort_unstable();
-
-    grapheme_clusters.join("")
+    grapheme_clusters
 }
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let lowered_word = word.to_lowercase();
     let target_word = grapheme_sort(&lowered_word);
 
-    let lowered_candidate_words: Vec<String> =
-        possible_anagrams.iter().map(|w| w.to_lowercase()).collect();
-    let lowered_and_sorted = lowered_candidate_words.iter().map(|x| grapheme_sort(&x));
-
-    lowered_and_sorted
-        .enumerate()
-        .fold(HashSet::new(), |mut acc, (i, candidate)| {
-            if candidate == target_word && lowered_word != lowered_candidate_words[i] {
-                acc.insert(possible_anagrams[i]);
-            }
-            acc
+    possible_anagrams
+        .iter()
+        .filter(|candidate| {
+            let lowered_candidate = candidate.to_lowercase();
+            lowered_candidate != lowered_word && grapheme_sort(&lowered_candidate) == target_word
         })
+        .copied()
+        .collect()
 }

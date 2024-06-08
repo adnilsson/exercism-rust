b380536ff001a71c2549aeddd49bff81b6db071c
Adrian Nilsson <adnilsson@users.noreply.github.com>
2024-06-08 16:55:38 +0200

    First solution to the Space Age exercise
    This does not use any macro to reduce boilerplate code. That's left to
    the second iteration.

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -1,19 +1,27 @@
 // The code below is a stub. Just enough to satisfy the compiler.
 // In order to pass the tests you can add-to or change any of this code.
 
+const EARTH_SECONDS_TO_YEAR: f64 = 1.0 / 31_557_600.0;
+
+/// A Duration on planet Earth
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    seconds: u64,
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        todo!("s, measured in seconds: {s}")
+        Self { seconds: s }
     }
 }
 
 pub trait Planet {
     fn years_during(d: &Duration) -> f64 {
-        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
+        let earth_years = d.seconds as f64 * EARTH_SECONDS_TO_YEAR;
+        earth_years / Self::orbital_period_in_earth_years()
     }
+
+    fn orbital_period_in_earth_years() -> f64;
 }
 
 pub struct Mercury;
@@ -25,11 +33,43 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    fn orbital_period_in_earth_years() -> f64 {
+        0.2408467
+    }
+}
+impl Planet for Venus {
+    fn orbital_period_in_earth_years() -> f64 {
+        0.61519726
+    }
+}
+impl Planet for Earth {
+    fn orbital_period_in_earth_years() -> f64 {
+        1.0
+    }
+}
+impl Planet for Mars {
+    fn orbital_period_in_earth_years() -> f64 {
+        1.8808158
+    }
+}
+impl Planet for Jupiter {
+    fn orbital_period_in_earth_years() -> f64 {
+        11.862615
+    }
+}
+impl Planet for Saturn {
+    fn orbital_period_in_earth_years() -> f64 {
+        29.447498
+    }
+}
+impl Planet for Uranus {
+    fn orbital_period_in_earth_years() -> f64 {
+        84.016846
+    }
+}
+impl Planet for Neptune {
+    fn orbital_period_in_earth_years() -> f64 {
+        164.79132
+    }
+}

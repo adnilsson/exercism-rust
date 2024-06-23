9fec9a89a4b311c9d2d05c143a417aae6be9491f
Adrian Nilsson <adnilsson@users.noreply.github.com>
2024-06-23 17:29:55 +0200

    Second iteration of the Sublist exercise
    Now more concise using a match expression.
    Inspired by the community solutions.

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -7,22 +7,12 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
-    let first_len = first_list.len();
-    let second_len = second_list.len();
-
-    if first_len == second_len && first_list == second_list {
-        return Comparison::Equal;
+    match (first_list.len(), second_list.len()) {
+        (n, m) if n < m && is_sublist(first_list, second_list) => Comparison::Sublist,
+        (n, m) if n > m && is_sublist(second_list, first_list) => Comparison::Superlist,
+        _ if first_list == second_list => Comparison::Equal,
+        _ => Comparison::Unequal,
     }
-
-    if first_len < second_len && is_sublist(first_list, second_list) {
-        return Comparison::Sublist;
-    }
-
-    if is_sublist(second_list, first_list) {
-        return Comparison::Superlist;
-    }
-
-    Comparison::Unequal
 }
 
 pub fn is_sublist<T: PartialEq>(candidate_sublist: &[T], candidate_superlist: &[T]) -> bool {

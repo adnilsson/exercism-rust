1e766fe0a05b66555b050bd9183709a2015f964e
Adrian Nilsson <adnilsson@users.noreply.github.com>
2024-06-23 17:13:29 +0200

    First solution to the Sublist exercise

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -6,6 +6,32 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    todo!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    let first_len = first_list.len();
+    let second_len = second_list.len();
+
+    if first_len == second_len && first_list == second_list {
+        return Comparison::Equal;
+    }
+
+    if first_len < second_len && is_sublist(first_list, second_list) {
+        return Comparison::Sublist;
+    }
+
+    if is_sublist(second_list, first_list) {
+        return Comparison::Superlist;
+    }
+
+    Comparison::Unequal
+}
+
+pub fn is_sublist<T: PartialEq>(candidate_sublist: &[T], candidate_superlist: &[T]) -> bool {
+    if candidate_sublist.len() == 0 {
+        // The empty list is a sublist of all lists.
+        // Also, .windows(size) panics if given size=0, so this must be handled explicitly.
+        return true;
+    }
+    candidate_superlist
+        .windows(candidate_sublist.len())
+        .any(|view| candidate_sublist == view)
 }

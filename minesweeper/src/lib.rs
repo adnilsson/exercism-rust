3ff45a1dbebe99618937494bb82bb9f1a009c5e6
Adrian Nilsson <adnilsson@users.noreply.github.com>
2024-08-10 19:13:40 +0200

    First solution to the Minesweeper exercise

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -1,15 +1,82 @@
+use std::collections::HashMap;
+
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    todo!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{minefield:#?}\n");
+    if minefield.is_empty() {
+        return vec![];
+    }
 
-    // Plan1:
-    // 1. iterate over rows
-    //  1.1 if element is a  mine, mark or increment the counter for  the previous and next elements in the row (may be oob)
-    // 2. repeat for columns intead of rows
+    // a minefield is guaranteed to be ASCII, so its safe to treat characters as u8
+    let mut ascii_minefield: Vec<Vec<u8>> =
+        minefield.iter().map(|&s| s.as_bytes().to_vec()).collect();
 
-    // Plan2:
-    // 1. write a function to compute all surronding coordinates given the coordinate of a mine
-    // 2. iterate over the boardl
-    //  2.1 if the element is a mine, compute surrounding coordinates and push to a vector
-    // 3. filter out oob coordinates, count duplicates
-    // 4. fill out result board
+    let mut mine_counter: HashMap<(_, _), u8> = HashMap::new();
+    for (i, &row) in minefield.iter().enumerate() {
+        let mine_positions = find_mines_linear(row.as_bytes());
+        for j in mine_positions {
+            let pos = neighborhood((i, j));
+            for mine_pos in pos {
+                mine_counter
+                    .entry(mine_pos)
+                    .and_modify(|mine_count| *mine_count += 1)
+                    .or_insert(1);
+            }
+        }
+    }
+
+    for (i, row) in minefield.iter().enumerate() {
+        let open_cells = row.char_indices().filter(|(_, c)| c.is_whitespace());
+        for (j, _) in open_cells {
+            if let Some(count) = mine_counter.get(&(i, j)) {
+                let elem: &mut u8 = ascii_minefield.get_mut(i).unwrap().get_mut(j).unwrap();
+                *elem = digit_to_ascii_codepoint(*count).unwrap();
+            }
+        }
+    }
+
+    ascii_minefield
+        .into_iter()
+        .map(|s| String::from_utf8(s).unwrap())
+        .collect()
+}
+
+fn find_mines_linear(mine_line: &[u8]) -> Vec<usize> {
+    mine_line
+        .iter()
+        .enumerate()
+        .filter(|c| *c.1 == b'*')
+        .map(|(positions, _)| positions)
+        .collect()
+}
+
+fn neighborhood(coordinate: (usize, usize)) -> Vec<(usize, usize)> {
+    let mut neighbors = vec![
+        (coordinate.0 + 1, coordinate.1),
+        (coordinate.0 + 1, coordinate.1 + 1),
+        (coordinate.0, coordinate.1 + 1),
+    ];
+
+    if coordinate.0 > 0 {
+        neighbors.extend_from_slice(&[
+            (coordinate.0 - 1, coordinate.1),
+            (coordinate.0 - 1, coordinate.1 + 1),
+        ]);
+    }
+    if coordinate.1 > 0 {
+        neighbors.extend_from_slice(&[
+            (coordinate.0, coordinate.1 - 1),
+            (coordinate.0 + 1, coordinate.1 - 1),
+        ]);
+    }
+    if coordinate.0 > 0 && coordinate.1 > 0 {
+        neighbors.push((coordinate.0 - 1, coordinate.1 - 1))
+    }
+
+    neighbors
+}
+
+fn digit_to_ascii_codepoint(i: u8) -> Option<u8> {
+    if i > 9 {
+        return None;
+    }
+    Some(i.to_string().as_bytes()[0])
 }

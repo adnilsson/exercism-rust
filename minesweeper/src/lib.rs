41dd7086d0acc8f7dae3d02162e865071bd736cb
Adrian Nilsson <adnilsson@users.noreply.github.com>
2024-10-26 17:09:26 +0200

    refactor: use helper functions and a type alias

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -1,15 +1,45 @@
 use std::collections::HashMap;
 
+type Coordinate = (usize, usize);
+
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
     if minefield.is_empty() {
         return vec![];
     }
 
+    // build map of coordinate -> #mines
+    let mine_count_map = build_mine_count_map(minefield);
+
+    annotate_minefield(minefield, &mine_count_map)
+        .into_iter()
+        .map(|s| String::from_utf8(s).unwrap())
+        .collect()
+}
+
+fn annotate_minefield(
+    minefield: &[&str],
+    mine_count_map: &HashMap<Coordinate, u8>,
+) -> Vec<Vec<u8>> {
     // a minefield is guaranteed to be ASCII, so its safe to treat characters as u8
     let mut ascii_minefield: Vec<Vec<u8>> =
         minefield.iter().map(|&s| s.as_bytes().to_vec()).collect();
 
-    let mut mine_counter: HashMap<(_, _), u8> = HashMap::new();
+    for (i, row) in minefield.iter().enumerate() {
+        let open_cells = row.char_indices().filter(|(_, c)| c.is_whitespace());
+        for (j, _) in open_cells {
+            if let Some(count) = mine_count_map.get(&(i, j)) {
+                let elem: &mut u8 = ascii_minefield.get_mut(i).unwrap().get_mut(j).unwrap();
+                *elem = digit_to_ascii_codepoint(*count).unwrap();
+            }
+        }
+    }
+
+    ascii_minefield
+}
+
+fn build_mine_count_map(minefield: &[&str]) -> HashMap<Coordinate, u8> {
+    let mut mine_counter = HashMap::new();
+
     for (i, &row) in minefield.iter().enumerate() {
         let mine_positions = find_mines_linear(row.as_bytes());
         for j in mine_positions {
@@ -23,20 +53,7 @@ pub fn annotate(minefield: &[&str]) -> Vec<String> {
         }
     }
 
-    for (i, row) in minefield.iter().enumerate() {
-        let open_cells = row.char_indices().filter(|(_, c)| c.is_whitespace());
-        for (j, _) in open_cells {
-            if let Some(count) = mine_counter.get(&(i, j)) {
-                let elem: &mut u8 = ascii_minefield.get_mut(i).unwrap().get_mut(j).unwrap();
-                *elem = digit_to_ascii_codepoint(*count).unwrap();
-            }
-        }
-    }
-
-    ascii_minefield
-        .into_iter()
-        .map(|s| String::from_utf8(s).unwrap())
-        .collect()
+    mine_counter
 }
 
 fn find_mines_linear(mine_line: &[u8]) -> Vec<usize> {
@@ -48,7 +65,7 @@ fn find_mines_linear(mine_line: &[u8]) -> Vec<usize> {
         .collect()
 }
 
-fn neighborhood(coordinate: (usize, usize)) -> Vec<(usize, usize)> {
+fn neighborhood(coordinate: Coordinate) -> Vec<Coordinate> {
     let mut neighbors = vec![
         (coordinate.0 + 1, coordinate.1),
         (coordinate.0 + 1, coordinate.1 + 1),
@@ -68,7 +85,7 @@ fn neighborhood(coordinate: (usize, usize)) -> Vec<(usize, usize)> {
         ]);
     }
     if coordinate.0 > 0 && coordinate.1 > 0 {
-        neighbors.push((coordinate.0 - 1, coordinate.1 - 1))
+        neighbors.push((coordinate.0 - 1, coordinate.1 - 1));
     }
 
     neighbors

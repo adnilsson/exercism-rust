044a3e11f92eaad4eb12a893b96b9696be9f463f
Adrian Nilsson <adnilsson@users.noreply.github.com>
2024-11-03 22:10:43 +0100

    First solution to the Luhn Trait exercise
    Very similar to the Luhn From exercise; the only difference is that we
    implement a trait on the calling type directly rather than box it in a
    Luhn struct.

--- luhn-trait/src/lib.rs
+++ luhn-trait/src/lib.rs
@@ -1,14 +1,14 @@
+mod luhn;
+
 pub trait Luhn {
     fn valid_luhn(&self) -> bool;
 }
 
-/// Here is the example of how to implement custom Luhn trait
-/// for the &str type. Naturally, you can implement this trait
-/// by hand for the every other type presented in the test suite,
-/// but your solution will fail if a new type is presented.
-/// Perhaps there exists a better solution for this problem?
-impl<'a> Luhn for &'a str {
+impl<T> Luhn for T
+where
+    T: ToString,
+{
     fn valid_luhn(&self) -> bool {
-        todo!("Determine if '{self}' is a valid credit card number.");
+        luhn::is_valid(self.to_string().as_str())
     }
 }

97bce06f8867fe7c239f70a4677331052497cb89
Adrian Nilsson <adnilsson@users.noreply.github.com>
2024-11-03 21:27:30 +0100

    First solution to the luhn_from exercise
    Any type that can be made into a string can be put into the Luhn type.

--- luhn-from/src/lib.rs
+++ luhn-from/src/lib.rs
@@ -1,18 +1,22 @@
-pub struct Luhn;
+mod luhn;
+
+pub struct Luhn {
+    code: String,
+}
 
 impl Luhn {
     pub fn is_valid(&self) -> bool {
-        todo!("Determine if the current Luhn struct contains a valid credit card number.");
+        luhn::is_valid(self.code.as_str())
     }
 }
 
-/// Here is the example of how the From trait could be implemented
-/// for the &str type. Naturally, you can implement this trait
-/// by hand for the every other type presented in the test suite,
-/// but your solution will fail if a new type is presented.
-/// Perhaps there exists a better solution for this problem?
-impl<'a> From<&'a str> for Luhn {
-    fn from(input: &'a str) -> Self {
-        todo!("From the given input '{input}' create a new Luhn struct.");
+impl<T> From<T> for Luhn
+where
+    T: ToString,
+{
+    fn from(input: T) -> Self {
+        Self {
+            code: input.to_string(),
+        }
     }
 }
---
+++ luhn-from/src/luhn.rs
@@ -0,0 +1,36 @@
+// Copied from the Luhn exercise
+
+/// Check a Luhn checksum.
+pub fn is_valid(code: &str) -> bool {
+    let Some(mut digits) = parse_input(code) else {
+        return false;
+    };
+
+    let multipliers = [1, 2].into_iter().cycle();
+    digits = digits
+        .into_iter()
+        .rev()
+        .zip(multipliers)
+        .map(|(digit, multiplier)| digit * multiplier)
+        .map(|prod_digit| (prod_digit / 10) + (prod_digit % 10))
+        .collect();
+
+    let checksum: u32 = digits.iter().sum();
+    checksum % 10 == 0
+}
+
+fn parse_input(input: &str) -> Option<Vec<u32>> {
+    let filtered_input: Vec<_> = input.chars().filter(|c| !c.is_ascii_whitespace()).collect();
+
+    if filtered_input.len() < 2 {
+        return None;
+    }
+
+    let digits: Vec<_> = filtered_input.into_iter().map(|c| c.to_digit(10)).collect();
+
+    if digits.iter().any(Option::is_none) {
+        return None;
+    }
+
+    Some(digits.into_iter().flatten().collect())
+}

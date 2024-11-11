21fcb78ffe83e7458ca3e9c7ee2b1a9dd934e7c4
Adrian Nilsson <adnilsson@users.noreply.github.com>
2024-11-11 22:06:17 +0100

    Second solution to the Hamming exercise
    Now, just use the standard zip but verify the length beforehand.
    I think this communicates the intent of the function better than
    catching a panic in the first solution.

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,11 +1,14 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    std::panic::catch_unwind(|| {
-        itertools::zip_eq(s1.chars(), s2.chars()).fold(0, |acc, pair| match pair {
+    if s1.len() != s2.len() {
+        return None;
+    }
+
+    std::iter::zip(s1.chars(), s2.chars())
+        .fold(0, |acc, pair| match pair {
             (a, b) if a != b => acc + 1,
             _ => acc,
         })
-    })
-    .ok()
+        .into()
 }

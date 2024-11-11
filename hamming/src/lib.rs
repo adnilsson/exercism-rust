47fb1f27e867bb0c34c9b9c5e2e18f4e8ea3662f
Adrian Nilsson <adnilsson@users.noreply.github.com>
2024-11-11 21:54:42 +0100

    First solution to the Hamming exercise
    This uses zip_eq from the crate itertools.
    I wanted a strict zip such as the one from python, but I'm not very
    happy to have to deal with panicking code.
    
    I'll probably settle for using the standard library zip, but checking
    the length beforehand.
    Keep this for future reference as an example of catch_unwind.

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,5 +1,11 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    todo!("What is the Hamming Distance between {s1} and {s2}");
+    std::panic::catch_unwind(|| {
+        itertools::zip_eq(s1.chars(), s2.chars()).fold(0, |acc, pair| match pair {
+            (a, b) if a != b => acc + 1,
+            _ => acc,
+        })
+    })
+    .ok()
 }
